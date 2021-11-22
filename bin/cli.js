b864319bf6ac28bb3ea7faf3338d31013d88f2e2
vinhactindi <nguyen.the.vinh@actindi.net>
2021-11-22 17:50:41 +0700

    add Pomodoro class

--- bin/cli.js
+++ bin/cli.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 
-const minimist = require('minimist')
+import minimist from 'minimist'
+import Pomodoro from '../lib/pomodoro.js'
 
 const args = process.argv.slice(2)
 
 const minimistOpts = {
-  alias: { p: 'pomodoro', s: 'short', l: 'long', h: 'help' },
-  default: { p: 25, s: 5, l: 15 },
+  alias: { f: 'focus', s: 'short', l: 'long', h: 'help' },
+  default: { f: 25, s: 5, l: 15 },
   unknown: (option) => {
     throw new Error('Unknown option ' + option)
   }
@@ -20,11 +21,15 @@ if (argv.help) {
 A simple CLI based focus supporting app.
 
 Options:
-  -p, --pomodoro <number> Specify the focus time (minutes) (default: 15)
+  -f, --focus <number>    Specify the focus time (minutes) (default: 15)
   -s, --short <number>    Specify the short break time (minutes) (default: 15)
   -t, --long <number>     Specify the long break time (minutes) (default: 500)
   -h, --help              Display help for command`
 
   console.log(usage)
   process.exit(0)
+} else {
+  const pomodoro = new Pomodoro(argv)
+
+  pomodoro.start()
 }
