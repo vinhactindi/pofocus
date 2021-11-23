aa066bd43a96c61eccc962e4af598dd98abcd4e1
vinhactindi <nguyen.the.vinh@actindi.net>
2021-11-23 10:43:59 +0700

    update usage

--- bin/cli.js
+++ bin/cli.js
@@ -30,9 +30,12 @@ if (argv.help) {
 A simple CLI based focus supporting app.
 
 Options:
+  -m, --message <string>  Specify the message of pomodoro cycle (minutes) 
   -f, --focus <number>    Specify the focus time (minutes) (default: 15)
   -s, --short <number>    Specify the short break time (minutes) (default: 15)
   -t, --long <number>     Specify the long break time (minutes) (default: 500)
+  -a, --analytics         Display all pomodoro cycle 
+  -r, --reset             Destroy all saved data
   -h, --help              Display help for command`
 
   console.log(usage)
